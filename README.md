16bac8dd40ad3a652b474174aae267fd04ad460b
Bhairav Mehta <bhairavmehta95@gmail.com>
2019-04-09 23:20:04 -0400

    adding locomotion

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # domain-randomizer
-A standalone library to randomize various OpenAI Gym Environments
+A standalone library to randomize various OpenAI Gym Environments.
+
+[Domain Randomization](https://arxiv.org/abs/1703.06907) is a idea that helps with sim2real transfer, but surprisingly has no general open source implementations. This library hopes to fill in that gap by providing a _standalone_ library that you can use in your own work.
+
+## Usage
+
--- randomizer/__init__.py
+++ randomizer/__init__.py
@@ -1,33 +1,50 @@
 from gym.envs.registration import register
-
-from common.envs.config import CONFIG_PATH
-from common.envs.lunar_lander import LunarLanderRandomized
 import os.path as osp
 
 register(
     id='LunarLanderDefault-v0',
-    entry_point='common.envs.lunar_lander:LunarLanderRandomized',
+    entry_point='randomizer.lunar_lander:LunarLanderRandomized',
     max_episode_steps=1000,
-    kwargs={'config': 'common/envs/config/LunarLanderRandomized/default.json'}
+    kwargs={'config': 'randomizer/config/LunarLanderRandomized/default.json'}
 )
 
 register(
     id='LunarLanderRandomized-v0',
-    entry_point='common.envs.lunar_lander:LunarLanderRandomized',
+    entry_point='randomizer.lunar_lander:LunarLanderRandomized',
     max_episode_steps=1000,
-    kwargs={'config': 'common/envs/config/LunarLanderRandomized/random.json'}
+    kwargs={'config': 'randomizer/config/LunarLanderRandomized/random.json'}
 )
 
 register(
     id='Pusher3DOFDefault-v0',
-    entry_point='common.envs.pusher3dof:PusherEnv3DofEnv',
+    entry_point='randomizer.pusher3dof:PusherEnv3DofEnv',
     max_episode_steps=100,
-    kwargs={'config': 'common/envs/config/Pusher3DOFRandomized/default.json'}
+    kwargs={'config': 'randomizer/config/Pusher3DOFRandomized/default.json'}
 )
 
 register(
     id='Pusher3DOFRandomized-v0',
-    entry_point='common.envs.pusher3dof:PusherEnv3DofEnv',
+    entry_point='randomizer.pusher3dof:PusherEnv3DofEnv',
     max_episode_steps=100,
-    kwargs={'config': 'common/envs/config/Pusher3DOFRandomized/random.json'}
+    kwargs={'config': 'randomizer/config/Pusher3DOFRandomized/random.json'}
+)
+
+register(
+    id='HumanoidRandomizedEnv-v0',
+    entry_point='common.envs.humanoid:HumanoidRandomizedEnv',
+    max_episode_steps=1000,
+    kwargs={
+        'config': 'randomizer/config/HumanoidRandomized/default.json',
+        'xml_name': 'humanoid.xml'
+    }
+)
+
+register(
+    id='HalfCheetahRandomizedEnv-v0',
+    entry_point='common.envs.half_cheetah:HalfCheetahRandomizedEnv',
+    max_episode_steps=1000,
+    kwargs={
+        'config': 'randomizer/config/HalfCheetahRandomized/default.json',
+        'xml_name': 'half_cheetah.xml'
+    }
 )
